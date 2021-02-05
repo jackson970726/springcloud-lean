d7a05be85c407e86338f9b6188aa29fecfee4dd3
JacksonZhuang <2773258845@qq.com>
2021-02-06 00:12:03 +0800

    eureka-security-server

---
+++ eureka-security-server/src/main/java/com/jackson/cloud/EurekaSecurityServerApplication.java
@@ -0,0 +1,13 @@
+package com.jackson.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSecurityServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaSecurityServerApplication.class, args);
+    }
+
+}

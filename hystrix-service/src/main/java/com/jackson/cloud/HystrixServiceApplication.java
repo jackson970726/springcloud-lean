df1f8e1b0f34961475568d8597b9acc208ec4e42
jackson970726 <2773258845@qq.com>
2021-02-06 10:27:03 +0800

    commit

---
+++ hystrix-service/src/main/java/com/jackson/cloud/HystrixServiceApplication.java
@@ -0,0 +1,15 @@
+package com.jackson.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class HystrixServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixServiceApplication.class, args);
+    }
+
+}
